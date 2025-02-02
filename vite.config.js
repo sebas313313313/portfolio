1bc79368a9cb0911ad477f789c5003e67aed4032
sebas313313313 <potoslipig8@gmail.com>
2025-02-01 20:45:07 -0500

    Update Vite config for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,10 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/portfolio/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    minify: true,
+  }
 })
